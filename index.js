49f73bc600661d3f30e32149a25c505f296d439d
Jonathan Gomez <rosemaria011@outlook.com>
2024-02-12 22:49:32 -0800

    added a pets website

---
+++ index.js
@@ -0,0 +1,94 @@
+import express from "express";
+import http from "node:http";
+import path from "node:path";
+import { createBareServer } from "@tomphttp/bare-server-node";
+
+const __dirname = process.cwd();
+const server = http.createServer();
+const app = express(server);
+const bareServer = createBareServer("/bare/");
+
+app.use(express.json());
+app.use(
+  express.urlencoded({
+    extended: true,
+  })
+);
+
+app.use(express.static(path.join(__dirname, "static")));
+app.get('/app', (req, res) => {
+  res.sendFile(path.join(process.cwd(), './static/app.html'));
+});
+app.get('/student', (req, res) => {
+  res.sendFile(path.join(process.cwd(), './static/loader.html'));
+});
+app.get('/apps', (req, res) => {
+  res.sendFile(path.join(process.cwd(), './static/apps.html'));
+});
+app.get('/gms', (req, res) => {
+  res.sendFile(path.join(process.cwd(), './static/gms.html'));
+});
+app.get('/lessons', (req, res) => {
+  res.sendFile(path.join(process.cwd(), './static/agloader.html'));
+});
+app.get('/credits', (req, res) => {
+  res.sendFile(path.join(process.cwd(), './static/credits.html'));
+});
+app.get('/partners', (req, res) => {
+  res.sendFile(path.join(process.cwd(), './static/partners.html'));
+});
+app.use((req, res) => {
+  res.statusCode = 404;
+  res.sendFile(path.join(process.cwd(), './static/404.html'))
+});
+
+server.on("request", (req, res) => {
+  if (bareServer.shouldRoute(req)) {
+    bareServer.routeRequest(req, res);
+  } else {
+    app(req, res);
+  }
+});
+
+server.on("upgrade", (req, socket, head) => {
+  if (bareServer.shouldRoute(req)) {
+    bareServer.routeUpgrade(req, socket, head);
+  } else {
+    socket.end();
+  }
+});
+
+let port = parseInt(process.env.PORT || "");
+
+if (isNaN(port)) port = 80;
+
+server.on("listening", () => {
+  const address = server.address();
+
+  // by default we are listening on 0.0.0.0 (every interface)
+  // we just need to list a few
+  console.log("Listening on:");
+  console.log(`\thttp://localhost:${address.port}`);
+  console.log(`\thttp://${hostname()}:${address.port}`);
+  console.log(
+    `\thttp://${
+      address.family === "IPv6" ? `[${address.address}]` : address.address
+    }:${address.port}`
+  );
+});
+
+// https://expressjs.com/en/advanced/healthcheck-graceful-shutdown.html
+process.on("SIGINT", shutdown)
+process.on("SIGTERM", shutdown)
+
+function shutdown() {
+  console.log('SIGTERM signal received: closing HTTP server')
+  server.close(() => {
+    console.log('Server closed.');
+    process.exit(0);
+  });
+}
+
+server.listen({
+  port,
+});
---
+++ static/assets/js/ag.js
@@ -0,0 +1,265 @@
+/*function youtube() {
+  let URL = 'https://www.youtube.com/';
+  let urlToInject = window.location.origin + __uv$config.prefix + __uv$config.encodeUrl(URL);
+  const newWindow = window.open();
+  const iframe = newWindow.document.createElement('iframe');
+  newWindow.document.body.style.margin = '0';
+  iframe.style.width = '100%';
+  iframe.style.height = '100%';
+  iframe.style.position = 'fixed';
+  iframe.style.top = '0';
+  iframe.style.left = '0';
+  iframe.style.zIndex = '99999';
+  iframe.style.border = 'none';
+  newWindow.document.body.style.overflow = 'hidden';
+  newWindow.document.body.appendChild(iframe);
+  iframe.src = urlToInject;
+}*/
+
+
+/*apps*/
+function gpt() {
+  agU = Ultraviolet.codec.xor.encode('https://ub7.org');
+  sessionStorage.setItem('agUrl', agU);
+  location.href = '/lessons';
+}
+
+function chess() {
+  agU = Ultraviolet.codec.xor.encode('https://chess.com');
+  sessionStorage.setItem('agUrl', agU);
+  location.href = '/lessons';
+}
+
+function discord() {
+  agU = Ultraviolet.codec.xor.encode('https://discord.com');
+  sessionStorage.setItem('agUrl', agU);
+  location.href = '/lessons';
+}
+
+function github() {
+  agU = Ultraviolet.codec.xor.encode('https://github.com');
+  sessionStorage.setItem('agUrl', agU);
+  location.href = '/lessons';
+}
+
+function google() {
+  agU = Ultraviolet.codec.xor.encode('https://google.com');
+  sessionStorage.setItem('agUrl', agU);
+  location.href = '/lessons';
+}
+
+function ng() {
+  agU = Ultraviolet.codec.xor.encode('https://nowgg.me/play/roblox-corporation/5349/roblox');
+  sessionStorage.setItem('agUrl', agU);
+  location.href = '/lessons';
+}
+
+function pin() {
+  agU = Ultraviolet.codec.xor.encode('https://pinterest.com');
+  sessionStorage.setItem('agUrl', agU);
+  location.href = '/lessons';
+}
+
+function reddit() {
+  agU = Ultraviolet.codec.xor.encode('https://reddit.com');
+  sessionStorage.setItem('agUrl', agU);
+  location.href = '/lessons';
+}
+
+function spotify() {
+  agU = Ultraviolet.codec.xor.encode('https://spotify.com');
+  sessionStorage.setItem('agUrl', agU);
+  location.href = '/lessons';
+}
+
+function tt() {
+  agU = Ultraviolet.codec.xor.encode('https://tiktok.com');
+  sessionStorage.setItem('agUrl', agU);
+  location.href = '/lessons';
+}
+
+function twitter() {
+  agU = Ultraviolet.codec.xor.encode('https://twitter.com');
+  sessionStorage.setItem('agUrl', agU);
+  location.href = '/lessons';
+}
+
+function yt() {
+  agU = Ultraviolet.codec.xor.encode('https://youtube.com');
+  sessionStorage.setItem('agUrl', agU);
+  location.href = '/lessons';
+}
+
+function y8() {
+  agU = Ultraviolet.codec.xor.encode('https://y8.com');
+  sessionStorage.setItem('agUrl', agU);
+  location.href = '/lessons';
+}
+
+function vscode() {
+  agU = Ultraviolet.codec.xor.encode('https://vscode.dev');
+  sessionStorage.setItem('agUrl', agU);
+  location.href = '/lessons';
+}
+
+/*games*/
+function onevone() {
+  agU = Ultraviolet.codec.xor.encode('https://1v1.lol');
+  sessionStorage.setItem('agUrl', agU);
+  location.href = '/lessons';
+}
+
+function basketBros() {
+  agU = Ultraviolet.codec.xor.encode('https://derpman.codeberg.page/echo/@main/basketbros-io/');
+  sessionStorage.setItem('agUrl', agU);
+  location.href = '/lessons';
+}
+
+function bitlife() {
+  agU = Ultraviolet.codec.xor.encode('https://derpman.codeberg.page/echo/@main/bitlife/index.html');
+  sessionStorage.setItem('agUrl', agU);
+  location.href = '/lessons';
+}
+
+function brebound() {
+  agU = Ultraviolet.codec.xor.encode('https://trinculo54.github.io/Boxel-rebound-hope/Newer/index.html');
+  sessionStorage.setItem('agUrl', agU);
+  location.href = '/lessons';
+}
+
+function cmg() {
+  agU = Ultraviolet.codec.xor.encode('https://coolmathgames.com');
+  sessionStorage.setItem('agUrl', agU);
+  location.href = '/lessons';
+}
+
+function crazygms() {
+  agU = Ultraviolet.codec.xor.encode('https://crazygames.com');
+  sessionStorage.setItem('agUrl', agU);
+  location.href = '/lessons';
+}
+
+function geforce() {
+  agU = Ultraviolet.codec.xor.encode('https://play.geforcenow.com');
+  sessionStorage.setItem('agUrl', agU);
+  location.href = '/lessons';
+}
+
+function holeio() {
+  agU = Ultraviolet.codec.xor.encode('https://hole-io.com');
+  sessionStorage.setItem('agUrl', agU);
+  location.href = '/lessons';
+}
+
+function jstris() {
+  agU = Ultraviolet.codec.xor.encode('https://jstris.jezevec10.com/');
+  sessionStorage.setItem('agUrl', agU);
+  location.href = '/lessons';
+}
+
+function twoZeroFourEight() {
+  agU = Ultraviolet.codec.xor.encode('https://derpmandev.github.io/unblocked-games/2048');
+  sessionStorage.setItem('agUrl', agU);
+  location.href = '/lessons';
+}
+
+function p2048() {
+  agU = Ultraviolet.codec.xor.encode('https://filipekiss.github.io/2048');
+  sessionStorage.setItem('agUrl', agU);
+  location.href = '/lessons';
+}
+
+function rbx() {
+  agU = Ultraviolet.codec.xor.encode('https://nowgg.me/play/roblox-corporation/5349/roblox');
+  sessionStorage.setItem('agUrl', agU);
+  location.href = '/lessons';
+}
+
+function slope() {
+  agU = Ultraviolet.codec.xor.encode('https://derpmandev.github.io/unblocked-games/slope');
+  sessionStorage.setItem('agUrl', agU);
+  location.href = '/lessons';
+}
+
+function subway() {
+  agU = Ultraviolet.codec.xor.encode('https://derpman.codeberg.page/echo/@main/subway-surfers/index.html');
+  sessionStorage.setItem('agUrl', agU);
+  location.href = '/lessons';
+}
+
+function territorial() {
+  agU = Ultraviolet.codec.xor.encode('https://territorial.io');
+  sessionStorage.setItem('agUrl', agU);
+  location.href = '/lessons';
+}
+
+function florr() {
+  agU = Ultraviolet.codec.xor.encode('https://florr.io');
+  sessionStorage.setItem('agUrl', agU);
+  location.href = '/lessons';
+}
+
+function cookieClicker() {
+  agU = Ultraviolet.codec.xor.encode('https://orteil.dashnet.org/cookieclicker');
+  sessionStorage.setItem('agUrl', agU);
+  location.href = '/lessons';
+}
+
+function rBowl() {
+  document.title = 'hound | V4';
+  var cloakcheckScript = document.createElement("script");
+  cloakcheckScript.src = "/assets/js/cloak.js";
+  document.head.appendChild(cloakcheckScript);
+
+  var functionsScript = document.createElement("script");
+  functionsScript.src = "/assets/js/functions.js";
+  document.head.appendChild(functionsScript);
+
+  document.body.innerHTML = `
+  <script src="/assets/js/index.js"></script>
+  <script src="/uv/uv.bundle.js"></script>
+  <script src="/uv/uv.bundle.js"></script>
+  <script src="/uv/uv.config.js"></script>
+  <script src="/assets/js/ag.js"></script>
+  <script>
+    window.navigator.serviceWorker
+      .register("/sw.js", {
+        scope: __uv$config.prefix,
+      });
+  </script>
+  <iframe src="/assets/rb/index.html" style="position: fixed; inset: 0px; outline: none; border: none; height: 100%; width: 100%; overflow: hidden;"></iframe>
+`;
+
+  function getRandomThreeDigitNumber() {
+      return Math.floor(Math.random() * 900) + 100;
+  }
+
+  function getRandomAlphanumericString(length) {
+      const characters = 'abcdefghijklmnopqrstuvw0123456789012345';
+      let result = '';
+      for (let i = 0; i < length; i++) {
+          result += characters.charAt(Math.floor(Math.random() * characters.length));
+      }
+      return result;
+  }
+
+  var randomThreeDigitNumber = getRandomThreeDigitNumber();
+  var randomAlphanumericString = getRandomAlphanumericString(15);
+
+  var url = '/temp?draft=' + randomAlphanumericString;
+  var title = 'Google Docs';
+
+  history.pushState({}, title, url);
+}
+
+function krunker() {
+  agU = Ultraviolet.codec.xor.encode('https://krunker.io');
+  sessionStorage.setItem('agUrl', agU);
+  location.href = '/lessons';
+}
+
+function paperIo() {
+  agU = Ultraviolet.codec.xor.encode('https://paper-io.com');
+  sessionStorage.setItem('agUrl', agU);
+  location.href = '/lessons';
+}
